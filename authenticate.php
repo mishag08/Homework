c5215f74f98f47a3fab952d6472587675fed87e0
mishag08 <125959825+mishag08@users.noreply.github.com>
2023-03-10 01:22:35 +0100

    Add files via upload

--- authenticate.php
+++ authenticate.php
@@ -1,42 +1,33 @@
 <html>
-	<head>
-		<title> Login Page </title>
-	</head>
+	<head></head>
+	
 	<body>
-	<nav>
-      <ul>
-        <li><a href="user-list.php">User List</a></li>
-        <li><a href="user-details.php">User Details</a></li>
-        <li><a href="user-add.php">Add User</a></li>
-      </ul>
-    </nav>
-		<h1> Login </h1>
-		<form action='login-form.php' method= 'post'>
-		<label for='username'> Username: </label>
-		<input type='text' id='username' name='username'>
-		<br>
-		<label for='password'> Password: </label>
-		<input type='password' id='password' name='password'>
-		<br>
-		<input type='submit' value='login'>
-	</form>
+		<form method='post' action='authenticate.php'>
+			Username: <input type='text' name='username'><br>
+			Password: <input type='password' name='password'>
+			<input type='submit' value='Login'>
+		</form>
 	</body>
-	<p>
-	<a href='user-list.php'> List of users 
-	</p>
+
 </html>
 
 <?php
 
-require_once 'login.php';
+//Example 12-2
+
+require_once "login.php";
+require_once "user-list.php";
+
 $conn = new mysqli($hn, $un, $pw, $db);
 if($conn->connect_error) die($conn->connect_error);
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
 	
+	//Get values from login screen
 	$tmp_username = mysql_entities_fix_string($conn, $_POST['username']);
 	$tmp_password = mysql_entities_fix_string($conn, $_POST['password']);
 	
+	//get password from DB w/ SQL
 	$query = "SELECT password FROM user WHERE username = '$tmp_username'";
 	
 	$result = $conn->query($query); 
@@ -52,14 +43,17 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 	
 	}
 	
+	//Compare passwords
 	if(password_verify($tmp_password,$passwordFromDB))
 	{
 		echo "successful login<br>";
 
 		session_start();
-		$_SESSION['username'] = $tmp_username;
 		
-		header("Location: user-add.php");
+		$user = new User($tmp_username);
+		$_SESSION['user'] = $user;
+		
+		header("Location: viewBook.php");
 	}
 	else
 	{
@@ -71,6 +65,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 $conn->close();
 
 
+//sanitization functions
 function mysql_entities_fix_string($conn, $string){
 	return htmlentities(mysql_fix_string($conn, $string));	
 }
